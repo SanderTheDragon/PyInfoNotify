e0f243040ff39846d6ad83e1254bd3ce1dc27c7c
SanderTheDragon <sanderthedragon@zoho.com>
2018-02-09 18:55:57 +0100

    Support for KNMI warnings

--- providers/knmi.py
+++ providers/knmi.py
@@ -9,6 +9,24 @@ class Provider(provider.Base):
         return True
     
     def check(self):
-        self.value = [ '1', '2', '3' ]
-        #self.get_html(self.data['url'])
-        pass
+        html = self.get_html(self.data['url'])
+        
+        for warning in html.findAll('div', { 'class': 'warning-overview' }):
+            if not warning.find('h3').text == 'Geen waarschuwingen voor':
+                message = ''
+                
+                color = ''
+                if 'warning-overview--yellow' in warning.attrs['class']:
+                    color = 'FFDE3C'
+                elif 'warning-overview--orange' in warning.attrs['class']:
+                    color = 'EF5D12'
+                elif 'warning-overview--red' in warning.attrs['class']:
+                    color = 'CE1F00'
+                
+                title = warning.find('h3')
+                message += '<img style=\"float: left;\" src=\"file://' + self.resource_path + 'knmi_' + title.attrs['class'][-1] + '.png\" />'
+                message += '<h2 style=\'margin: 0 40px; float: left; color: #' + color + ';\'>' + title.text + '</h2>'
+                
+                message += warning.find('div', { 'class': 'warning-overview__description' }).text
+                
+                self.value.append(message)
