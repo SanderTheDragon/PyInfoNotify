4ee64fba169f966450924e875012ea7fb361df65
SanderTheDragon <sanderthedragon@zoho.com>
2018-02-27 15:28:26 +0100

    Added time information to KNMI notifications

--- providers/knmi.py
+++ providers/knmi.py
@@ -32,6 +32,17 @@ class Provider(provider.Base):
                 message += '<h2 style=\'margin: 0 40px; float: left; color: #' + color + ';\'>' + title.text + '</h2><br/>'
                 
                 message += warning.find('div', { 'class': 'warning-overview__description' }).decode_contents()
-                message = message[:-5] #Remove last <br/>
+                
+                timelines = warning.findAll('div', { 'class': 'warning-timeline__section' })
+                if len(timelines) > 0:
+                    if not '<p>' in message: #A P tag already adds enough spacing
+                        message += '<br/>'
+                    
+                    days = warning.findAll('div', { 'class': 'warning-timeline__day' })
+                    for timeline in timelines:
+                        if not '<p>' in message: #A P tag already adds enough spacing
+                            message += '<br/>'
+                        
+                        message += 'Vanaf ' + days[int(timeline.attrs['data-from-day'])].text + ' ' + timeline.attrs['data-from-time'] + ' tot ' + days[int(timeline.attrs['data-until-day'])].text + timeline.attrs['data-until-time']
                 
                 self.values.append(message)
